f7306286b8d15ff54d7dca7dc4170f55bc464e72
inkfarer <inkfarer@gmail.com>
2020-01-07 05:47:07 +0200

    rudimentary graphics and updated package

--- graphics/main.js
+++ graphics/main.js
@@ -1,1 +1,12 @@
-document.getElementById("fun").innerText="?"
+const scoreA = nodecg.Replicant('scoreA', {defaultValue: 0});
+const scoreB = nodecg.Replicant('scoreB', {defaultValue: 0});
+const nameA = nodecg.Replicant('nameA', {defaultValue: ""});
+const nameB = nodecg.Replicant('nameB', {defaultValue: ""});
+const flavorText = nodecg.Replicant('flavorText', {defaultValue: ""});
+
+scoreA.on('change', (newValue) => {
+    p1score.innerText = newValue;
+})
+scoreB.on('change', (newValue) => {
+    p2score.innerText = newValue;
+})
